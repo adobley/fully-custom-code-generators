0b1c42c58308e00297973c81e97169daa1cd4da5
Jeffrey Matthias <jeffrey@matthias.org>
2025-01-25 14:32:15 -0700

    First pass refactors on npc test.

--- test/core/actors/npc_test.exs
+++ test/core/actors/npc_test.exs
@@ -1,57 +1,79 @@
 defmodule Core.Actors.NPCTest do
   use ArcaneAssist.DataCase
 
-  alias Core.Actors
+  alias Core.Actors.NPC
 
-  describe "npcs" do
-    import ArcaneAssist.ActorsFixtures
+  import ArcaneAssist.ActorsFixtures
 
-    @invalid_attrs %{name: nil}
+  @invalid_attrs %{name: DateTime.utc_now()}
 
-    test "list_npcs/0 returns all npcs" do
+  describe "list/npcs/0" do
+    test "success: returns all npcs" do
       npc = npc_fixture()
-      assert Actors.NPC.list_npcs() == [npc]
+      assert NPC.list_npcs() == [npc]
     end
+  end
 
-    test "get_npc!/1 returns the npc with given id" do
+  describe "get_npc!/1" do
+    test "success: returns the npc with given id" do
       npc = npc_fixture()
-      assert Actors.NPC.get_npc!(npc.id) == npc
+      assert NPC.get_npc!(npc.id) == npc
     end
+  end
 
-    test "create_npc/1 with valid data creates a npc" do
+  describe "create_npc/1" do
+    test "success: with valid data creates a npc" do
       valid_attrs = %{name: "some name"}
 
-      assert {:ok, %Schema.NPC{} = npc} = Actors.NPC.create_npc(valid_attrs)
+      assert {:ok, %Schema.NPC{} = npc} = NPC.create_npc(valid_attrs)
       assert npc.name == "some name"
     end
 
-    test "create_npc/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = Actors.NPC.create_npc(@invalid_attrs)
+    test "success: with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = NPC.create_npc(@invalid_attrs)
     end
+  end
 
-    test "update_npc/2 with valid data updates the npc" do
+  describe "update_npc/2" do
+    test "success: with valid data updates the npc" do
       npc = npc_fixture()
       update_attrs = %{name: "some updated name"}
 
-      assert {:ok, %Schema.NPC{} = npc} = Actors.NPC.update_npc(npc, update_attrs)
+      assert {:ok, %Schema.NPC{} = npc} = NPC.update_npc(npc, update_attrs)
       assert npc.name == "some updated name"
     end
 
-    test "update_npc/2 with invalid data returns error changeset" do
+    test "error: returns changeset with errors" do
       npc = npc_fixture()
-      assert {:error, %Ecto.Changeset{}} = Actors.NPC.update_npc(npc, @invalid_attrs)
-      assert npc == Actors.NPC.get_npc!(npc.id)
+      assert {:error, %Ecto.Changeset{}} = NPC.update_npc(npc, @invalid_attrs)
+      assert npc == NPC.get_npc!(npc.id)
+    end
+  end
+
+  describe "delete_npc/1" do
+    test "success: deletes the npc" do
+      npc = npc_fixture()
+      assert {:ok, %Schema.NPC{}} = NPC.delete_npc(npc)
+      assert_raise Ecto.NoResultsError, fn -> NPC.get_npc!(npc.id) end
+    end
+  end
+
+  describe "change_npc/1" do
+    test "success: returns a npc changeset" do
+      npc = npc_fixture()
+      assert %Ecto.Changeset{} = NPC.change_npc(npc)
     end
 
-    test "delete_npc/1 deletes the npc" do
+    test "error: returns invalid changeset when given bad data" do
       npc = npc_fixture()
-      assert {:ok, %Schema.NPC{}} = Actors.NPC.delete_npc(npc)
-      assert_raise Ecto.NoResultsError, fn -> Actors.NPC.get_npc!(npc.id) end
-    end
 
-    test "change_npc/1 returns a npc changeset" do
-      npc = npc_fixture()
-      assert %Ecto.Changeset{} = Actors.NPC.change_npc(npc)
+      assert changeset = %Ecto.Changeset{valid?: false} = NPC.change_npc(npc, @invalid_attrs)
+
+      errors = errors_on(changeset)
+
+      for {field, _} <- @invalid_attrs do
+        assert Enum.member?(errors, {field, ["is invalid"]})
+      end
     end
   end
 end
